8ca1a529525f89cf3dbe594dc484213412e46b46
ramazan <rmzns1515@gmail.com>
2019-04-04 18:40:24 +0300

    mongoose framwork eklendi

---
+++ ExpressBackend/app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json());
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb+srv://ferat:ferat123@cluster0-wgidr.mongodb.net/test?retryWrites=true';
+
+mongo.git s(url, (err)=>{
+  console.log('Terhubung ke database!')
+})
+
+app.get('/data', (req, res)=>{
+  mongo.connect(url, (err, db)=>{
+    var collection = db.collection('ninja');
+    collection.find({}).toArray((x, hasil)=>{
+      res.send(hasil);
+    })
+  })
+})
+
+app.post('/data', (req, res)=>{
+  mongo.connect(url, (err, db)=>{
+    var collection = db.collection('ninja');
+    var sesuatu = {
+      nama: req.body.nama,
+      usia: req.body.usia
+    }
+    collection.insert(sesuatu, (x, hasil)=>{
+      res.send(hasil);
+    })
+  })
+})
+
+app.listen(3210, ()=>{
+  console.log('Server aktif @port 3210!');
+})
